260ebe7c72b73392cfb5b9af3dbc85983836f5e4
Wil <wfkoch@gmail.com>
2018-03-04 16:12:48 -0500

    better dir name

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
-RESULT_HOME=/home/wil/workspace/buflightdev/projects/neurocontroller/att-sitl/results/baselines/ddpg-abs_pid_penalty-window_size2-2
+BASELINE_DIR=$(pwd)
+GYM_HOME=/home/wil/workspace/buflightdev/projects/gym-flightcontrol
+cd $GYM_HOME
+COMMIT=$(git describe --always)
+ENV=attitude-zero-start-v0
+RESULT_HOME=/home/wil/workspace/buflightdev/projects/neurocontroller/att-sitl/results/baselines/ALG=ddpg-IN=error_x3-ENV=${COMMIT}_${ENV}-REWARD=euclidean_gaussian_penalty
 export OPENAI_LOGDIR=$RESULT_HOME/logs
-python3 -m baselines.ddpg.main --env-id="flightcontrol-v0" \
+
+cd $BASELINE_DIR
+python3 -m baselines.ddpg.main --env-id=$ENV \
  --ckpt-dir=$RESULT_HOME/checkpoints \
  --nb-epochs=5000 \
  --progress-dir=$RESULT_HOME/model-progress
- #--nb-epoch-cycles=2 \
- #--nb-rollout-steps=3 \
