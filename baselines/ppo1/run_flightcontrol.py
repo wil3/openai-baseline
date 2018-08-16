e7412199fdbc7b79510db9a688f5d8036279132c
Wil <wfkoch@gmail.com>
2018-08-16 10:33:04 -0400

    allow more hyper params at cli

--- baselines/ppo1/run_flightcontrol.py
+++ baselines/ppo1/run_flightcontrol.py
@@ -14,7 +14,7 @@ from baselines.ppo1.mlp_policy import MlpPolicy
 from baselines.common import set_global_seeds
 
 def train(env_id, num_timesteps, seed, flight_log_dir, ckpt_dir, render, restore_dir,ckpt_freq, 
-          optim_stepsize=3e-4, schedule="linear", gamma=0.99):
+          optim_stepsize=3e-4, schedule="linear", gamma=0.99, optim_epochs=10, optim_batchsize=64):
     from baselines.ppo1 import pposgd_simple
     import baselines.common.tf_util as U
     sess = U.single_threaded_session()
@@ -42,12 +42,11 @@ def train(env_id, num_timesteps, seed, flight_log_dir, ckpt_dir, render, restore
             max_timesteps=num_timesteps,
             timesteps_per_actorbatch=2048,
             clip_param=0.2, entcoeff=0.0,
-            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
-            gamma=0.99, lam=0.95, schedule='linear',
+            optim_epochs=optim_epochs, optim_stepsize=optim_stepsize, optim_batchsize=optim_batchsize,
+            gamma=0.99, lam=0.95, schedule=schedule,
             flight_log = flight_log,
             ckpt_dir = ckpt_dir,
             restore_dir = restore_dir,
-            #model_ckpt_path = model_ckpt_path
             save_timestep_period= ckpt_freq
             )
     env.close()
@@ -58,14 +57,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('envid', type=str)
     parser.add_argument('--num-timesteps', type=int, default=1e7)
-    parser.add_argument('--stepsize', type=float, default=3e-4)
-    parser.add_argument('--schedule', type=str, default="linear", help="One of constant, linear")
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--flight-log-dir', type=str, default=None)
     parser.add_argument('--ckpt-dir', type=str, default=None)
     parser.add_argument('--restore-dir', help="If we should restore a graph", type=str, default=None)
     parser.add_argument('--render', action="store_true")
     parser.add_argument('--ckpt-freq', help='Timestep frequency checkpoints are made', type=int, default=1000)
+
+    # Hyper parameters
+    parser.add_argument('--stepsize', type=float, default=3e-4)
+    parser.add_argument('--schedule', type=str, default="linear", help="One of constant, linear")
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batchsize', type=int, default=64)
 
     args = parser.parse_args()
     train(args.envid, args.num_timesteps, args.seed, args.flight_log_dir,
