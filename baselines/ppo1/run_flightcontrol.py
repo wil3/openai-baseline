520c5d1921eabe05a31eb27d882a9114ed902866
Wil Koch <wfkoch@gmail.com>
2018-10-19 15:16:04 -0400

    add more options

--- baselines/ppo1/run_flightcontrol.py
+++ baselines/ppo1/run_flightcontrol.py
@@ -14,7 +14,7 @@ from baselines.ppo1.mlp_policy import MlpPolicy
 from baselines.common import set_global_seeds
 
 def train(env_id, num_timesteps, seed, flight_log_dir, ckpt_dir, render, restore_dir,ckpt_freq, 
-          optim_stepsize=3e-4, schedule="linear", gamma=0.99, optim_epochs=10, optim_batchsize=64):
+          optim_stepsize=3e-4, schedule="linear", gamma=0.99, optim_epochs=10, optim_batchsize=64, horizon=2048):
     from baselines.ppo1 import pposgd_simple
     import baselines.common.tf_util as U
     sess = U.single_threaded_session()
@@ -40,7 +40,7 @@ def train(env_id, num_timesteps, seed, flight_log_dir, ckpt_dir, render, restore
     set_global_seeds(workerseed)
     pposgd_simple.learn(env, policy_fn,
             max_timesteps=num_timesteps,
-            timesteps_per_actorbatch=2048,
+            timesteps_per_actorbatch=horizon,
             clip_param=0.2, entcoeff=0.0,
             optim_epochs=optim_epochs, optim_stepsize=optim_stepsize, optim_batchsize=optim_batchsize,
             gamma=0.99, lam=0.95, schedule=schedule,
@@ -69,8 +69,9 @@ if __name__ == '__main__':
     parser.add_argument('--schedule', type=str, default="linear", help="One of constant, linear")
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--batchsize', type=int, default=64)
+    parser.add_argument('--horizon', type=int, default=2048)
 
     args = parser.parse_args()
     train(args.envid, args.num_timesteps, args.seed, args.flight_log_dir,
-          args.ckpt_dir,  args.render, args.restore_dir, args.ckpt_freq, schedule=args.schedule, optim_stepsize=args.stepsize)
+          args.ckpt_dir,  args.render, args.restore_dir, args.ckpt_freq, schedule=args.schedule, optim_stepsize=args.stepsize, horizon=args.horizon, optim_batchsize=args.batchsize)
 
